db235c16e5c70b57a20e86676da54ca6ba5dba28
gdonofrio <gian.donofrio2000@gmail.com>
2022-09-05 20:23:27 -0300

    first commit

---
+++ routes/new.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const sgMail = require('../services/sendgrid');
+const router = express.Router();
+
+router.post('/api/mail', async (req, res) => {
+  const { to, subject, text, html, sandboxMode = false } = req.body;
+  const msg = {
+    to,
+    from: 'gian.donofrio2000@gmail.com',
+    subject,
+    text,
+    html,
+    mail_settings: {
+      sandbox_mode: {
+        enable: sandboxMode,
+      },
+    },
+  };
+  try {
+    await sgMail.send(msg);
+    res.status(200).json
+  } catch (error) {
+    res.status(error.code).json(error.message);
+  }
+
+  res.status(201).send({ success: true });
+});
+
+module.exports = router;
