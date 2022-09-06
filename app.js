c4d5388821c1b21d92ed9dcd6008a87058a4f2a1
gdonofrio <gian.donofrio2000@gmail.com>
2022-09-05 21:54:49 -0300

    adding cors

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 
-app.use(cors({ origin: '*' }));
+app.use(cors());
 
 app.get('/*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   res.sendFile(path.join(__dirname, 'client', 'index.html'));
+  next();
 });
 
 app.use(json());
