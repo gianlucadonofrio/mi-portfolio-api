1ed77984f7cc984e40889077ef8ac0c5e15d0d01
gdonofrio <gian.donofrio2000@gmail.com>
2022-09-05 21:45:57 -0300

    adding cors

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const { json } = require('body-parser');
 const createMail = require('./routes/new');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'index.html'));
-});
+app.use(cors());
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
 
 app.use(json());
 app.use(createMail);
